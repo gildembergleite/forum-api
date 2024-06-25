ebb131f9ea4d0c45e811e0e81a2bca957241fcdc
gildembergleite <gildembergleite@gmail.com>
2024-06-25 12:18:39 -0300

    refactor: change id types in entities and add new field in constructor props

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,9 +1,12 @@
 import { Entity } from '../core/entities/entity'
+import { UniqueEntityId } from './value-objects/unique-entity-id'
 
 export interface AnswerConstructorProps {
   content: string
-  instructorId: string
-  questionId: string
+  instructorId: UniqueEntityId
+  questionId: UniqueEntityId
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Answer extends Entity<AnswerConstructorProps> {
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,15 @@
 import { Entity } from '../core/entities/entity'
 import { Slug } from './value-objects/slug'
+import { UniqueEntityId } from './value-objects/unique-entity-id'
 
 export interface QuestionConstructorProps {
   title: string
   slug: Slug
   content: string
-  studentId: string
+  studentId: UniqueEntityId
+  bestAnswerId?: UniqueEntityId
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Question extends Entity<QuestionConstructorProps> {
