69c4a867f14edca8ed896af853caaa3e80e33377
gildembergleite <gildembergleite@gmail.com>
2024-06-25 12:13:06 -0300

    feat: created unique entity id object value

--- src/domain/core/entities/entity.ts
+++ src/domain/core/entities/entity.ts
@@ -1,15 +1,15 @@
-import { randomUUID } from 'node:crypto';
+import { UniqueEntityId } from '../../entities/value-objects/unique-entity-id';
 
 export class Entity<T> {
-  private _id: string
+  private _id: UniqueEntityId
   protected props: T
 
-  get id(): string {
+  get id() {
     return this._id
   }
 
   constructor({ id, props }: { id?: string; props: T }) {
-    this._id = id ?? randomUUID()
+    this._id = new UniqueEntityId({ value: id })
     this.props = props
   }
 }
---
+++ src/domain/entities/value-objects/unique-entity-id.test.ts
@@ -0,0 +1,21 @@
+import { validate as isUUID, v4 as uuid } from 'uuid';
+import { describe, expect, it } from 'vitest';
+import { UniqueEntityId } from './unique-entity-id';
+
+describe('create UniqueEntityId', () => {
+  it('should generate a unique UUID when value is not provided', () => {
+    const entityId = new UniqueEntityId({})
+    expect(isUUID(entityId.toValue())).toBe(true)
+  })
+
+  it('should use the given value when specified', () => {
+    const generateUUID = uuid()
+    const entityId = new UniqueEntityId({ value: generateUUID })
+    expect(entityId.toValue()).toBe(generateUUID)
+  })
+
+  it('should return the correct value using the getter', () => {
+    const entityId = new UniqueEntityId({})
+    expect(isUUID(entityId.toValue())).toBe(true)
+  })
+})
