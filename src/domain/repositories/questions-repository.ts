19d22d6586a3d208a4514c33fbe072f135fe8d25
gildembergleite <gildembergleite@gmail.com>
2024-06-26 17:37:27 -0300

    feat: add delete by question id in Question Repository

--- src/domain/repositories/questions-repository.ts
+++ src/domain/repositories/questions-repository.ts
@@ -3,4 +3,5 @@ import { Question } from '@/domain/entities/question'
 export interface QuestionsRepository {
   create(Question: Question): Promise<void>
   findBySlug(slug: string): Promise<Question | null>
+  deleteByQuestionId(id: string): Promise<Question | null>
 }
