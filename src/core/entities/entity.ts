8f084bd2e3ffdc96e665e499f838e6cd51176d3c
gildembergleite <gildembergleite@gmail.com>
2024-06-25 21:06:57 -0300

    refactor: add protected in Entity constructor

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -2,14 +2,13 @@ import { UniqueEntityId } from './unique-entity-id'
 
 export class Entity<T> {
   private _id: UniqueEntityId
-
   protected props: T
 
   get id() {
     return this._id
   }
 
-  constructor({ id, props }: { props: T; id?: UniqueEntityId }) {
+  protected constructor({ id, props }: { props: T; id?: UniqueEntityId }) {
     this._id = id ?? new UniqueEntityId()
 
     this.props = props
